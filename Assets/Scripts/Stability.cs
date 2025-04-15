3ff1b53b89efaa082983b912eeb26112794cf351
George Jimy <knivespeans@gmail.com>
2025-04-15 17:49:42 -0500

    Bicicleta

---
+++ Assets/Scripts/Stability.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stability : MonoBehaviour
+{
+    public float smoothSpeed = 2f;
+
+    void Update()
+    {
+        Vector3 currentEuler = transform.eulerAngles;
+        float newZ = Mathf.LerpAngle(currentEuler.z, 0f, Time.deltaTime * smoothSpeed);
+        transform.eulerAngles = new Vector3(currentEuler.x, currentEuler.y, newZ);
+    }
+}
